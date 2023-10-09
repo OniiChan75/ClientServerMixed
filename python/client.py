eedd390164b23035f251be65dcb93d0429c48de9
SuzuKanae <melesgabriele@gmail.com>
2023-10-09 08:23:11 +0200

    initial commit

---
+++ python/client.py
@@ -0,0 +1,26 @@
+import socket
+
+host = "127.0.0.1"
+port = 8001
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+
+latitudine1 = 45.4219
+longitudine1 = 9.1342
+latitudine2 = 41.8919
+longitudine2 = 12.4958
+
+# data = [latitudine1, longitudine1, latitudine2, longitudine2]
+# bytes = b"".join([bytes(str(dato), "utf-8") for dato in data])
+# print(bytes)
+# s.sendall(bytes)
+s.send(latitudine1.encode())
+s.send(longitudine1.encode())
+s.send(latitudine2.encode())
+s.send(longitudine2.encode())
+
+data = s.recv(1024)
+print(data.decode())
+
+s.close()
