e661fb49d91daea701493dc9fd109b0cdb068216
Giorgio Tentorio <giorgiotentorio@gmail.com>
2023-10-09 09:27:57 +0200

    fixed server java

--- java/Server/src/App.java
+++ java/Server/src/App.java
@@ -1,56 +1,57 @@
 import java.io.*;
 import java.net.*;
+import java.lang.Math;
 
 public class App {
     public static void main(String[] args) {
         DatagramSocket socket = null;
 
         try {
-            // Crea una socket UDP sulla porta 9876 with a timeout of 60 seconds
+            // Create a UDP socket on port 8001 with a timeout of 60 seconds
             socket = new DatagramSocket(8001);
-            socket.setSoTimeout(60000);
+            socket.setSoTimeout(60000); // Set timeout to 60 seconds
 
             while (true) {
                 byte[] receiveData = new byte[1024];
 
-                // Crea un pacchetto DatagramPacket per ricevere i dati
+                // Create a DatagramPacket to receive data
                 DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 
-                // Ricevi il pacchetto dal client
+                // Receive the packet from the client
                 socket.receive(receivePacket);
 
-
-                // Estrai i dati dalla ricezione
+                // Extract the data from the reception
                 String receivedData = new String(receivePacket.getData(), 0, receivePacket.getLength());
                 System.out.println(receivedData);
 
-                // Dividi le coordinate dalla stringa ricevuta
+                // Split the coordinates from the received string
                 String[] coordinates = receivedData.split(";");
                 // System.out.println("Coordinate ricevute: " + coordinates);
 
                 if (coordinates.length != 2) {
                     System.out.println("Pacchetto non valido: " + receivedData);
                 } else {
-                    // Ottieni le coordinate x e y
+                    // Extract and process coordinates using Haversine formula
                     String[] coordinates1 = coordinates[0].split(",");
-                    float x1 = Float.parseFloat(coordinates1[0]);
-                    float y1 = Float.parseFloat(coordinates1[1]);
+                    double lat1 = Double.parseDouble(coordinates1[0]);
+                    double lon1 = Double.parseDouble(coordinates1[1]);
 
                     String[] coordinates2 = coordinates[1].split(",");
-                    float x2 = Float.parseFloat(coordinates2[0]);
-                    float y2 = Float.parseFloat(coordinates2[1]);
+                    double lat2 = Double.parseDouble(coordinates2[0]);
+                    double lon2 = Double.parseDouble(coordinates2[1]);
 
-                    // Crea una risposta con le coordinate ricevute
-                    // String response = "Coordinate ricevute: X = " + x1 + ", Y = " + y1 + "\nCoordinate ricevute: X = " + x2 + ", Y = " + y2;
-                    float distance = (float) Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-                    String response = String.valueOf(distance);
+                    // Calculate the distance using Haversine formula
+                    double distance = calculateHaversineDistance(lat1, lon1, lat2, lon2);
+
+                    // Create a response string
+                    String response = "Distance between the two points: " + distance + " kilometers";
                     byte[] responseData = response.getBytes();
 
-                    // Crea un pacchetto DatagramPacket per inviare la risposta al client
+                    // Create a DatagramPacket to send the response to the client
                     DatagramPacket sendPacket = new DatagramPacket(responseData, responseData.length,
                             receivePacket.getAddress(), receivePacket.getPort());
 
-                    // Invia la risposta al client
+                    // Send the response to the client
                     socket.send(sendPacket);
                 }
             }
@@ -61,5 +62,17 @@ public class App {
                 socket.close();
             }
         }
+    }
+
+    // Function to calculate distance using Haversine formula
+    public static double calculateHaversineDistance(double lat1, double lon1, double lat2, double lon2) {
+        double R = 6371; // Earth's radius in kilometers
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLon = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+                 + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                 * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return R * c; // Distance in kilometers
     }
 }
