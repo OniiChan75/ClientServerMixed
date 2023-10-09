d020c9092033c6fecc256c35800b23d2be52df24
SuzuKanae <melesgabriele@gmail.com>
2023-10-09 09:33:29 +0200

    English translations client.py

--- python/client.py
+++ python/client.py
@@ -1,10 +1,10 @@
 import socket
 
-# Indirizzo IP e porta del server
+# Server IP address and port
 server_ip = '192.168.1.61'
 server_port = 8001
 
-# Crea un socket UDP
+# Create a UDP socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 timeout = 100.0
@@ -12,35 +12,32 @@ client_socket.settimeout(timeout)
 
 try:
     client_socket.connect((server_ip, server_port))
-    print("Connesso al server.")
+    print("Connected to the server.")
     
-    # Input delle coordinate dal client (lat, lon ; lat, lon)
-    latituine1 = 45.4642700
-    longitudine1 = 9.1895100
-    latituine2 = 45.8558900
-    longitudine2 = 9.3970400
+    # Coordinate input from client (lat, lon ; lat, lon)
+    latitude1 = input("Enter latitude1: ")
+    longitude1 = input("Enter longitude1: ")
+    latitude2 = input("Enter latitude2: ")
+    longitude2 = input("Enter longitude2: ")
         
-    # Crea una stringa con le coordinate
-    coordinate = f"{latituine1},{longitudine1};{latituine2},{longitudine2}"
-    print(coordinate)
+    # Create a string with coordinates
+    coordinates = f"{latitude1},{longitude1};{latitude2},{longitude2}"
+    print(coordinates)
 
-    # Invia i dati al server
-    client_socket.sendall(coordinate.encode())
+    # Send data to server
+    client_socket.sendall(coordinates.encode())
 
     try:
-        # Ricevi la risposta dal server
+        # Get response from server
         response, server_address = client_socket.recvfrom(1024)
-        risultato = response.decode()
-        #if(float(risultato) < 0):
-        #    risultato = risultato * -1
-        #print("Distanza tra i due punti:" + risultato + " km")
-        print(risultato)
+        result = response.decode()
+        print(result)
                       
     except socket.timeout:
-        print("Timeout scaduto. Riprova.")
+        print("Timeout expired. Try again.")
         
 except KeyboardInterrupt:
-    print("Client interrotto.")
+    print("Client stopped.")
 
 finally:
     client_socket.close()
